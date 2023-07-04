029fd971412ccd0e353f12ee4da1d0d835e106bf
SayyadKarishma <sdkarishma555@gmail.com>
2023-07-04 11:57:20 +0530

    first commit

---
+++ src/main/java/com/demo/Demospringbootamdocs3Application.java
@@ -0,0 +1,13 @@
+package com.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Demospringbootamdocs3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Demospringbootamdocs3Application.class, args);
+	}
+
+}
